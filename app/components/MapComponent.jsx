e3e6f2cb7c145699759b71d19b08775b05b60b00
rikiruswandii <rikiruswandi28@gmail.com>
2025-08-21 16:00:34 +0700

    fix:leaflet issue

--- app/components/MapComponent.jsx
+++ app/components/MapComponent.jsx
@@ -1,24 +1,33 @@
-"use client"; // pastikan ini ada supaya Next.js tahu ini komponen client
-
-import { useEffect } from "react";
-import dynamic from "next/dynamic";
-
-// Import library Leaflet **hanya di client**
-const L = dynamic(() => import("leaflet"), { ssr: false });
+import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import React, { useEffect } from "react";
+import Popup from "./PopupHandler";
+import {
+  MapContainer,
+  TileLayer,
+  FeatureGroup,
+  ScaleControl,
+  useMap,
+} from "react-leaflet";
 import "@geoman-io/leaflet-geoman-free";
 import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
-
-// Import komponen internal
-import Popup from "./PopupHandler";
 import FitBoundsComponent from "./FitBound";
 import LeafletControlGeocoder from "./Geocoder";
 import DrawComponent from "./DrawMap";
-
-import { MapContainer, TileLayer, FeatureGroup, ScaleControl, useMap } from "react-leaflet";
 import { createRoot } from "react-dom/client";
 
-const PopupHandlerClient = ({ geoJsonData }) => {
+// Konfigurasi icon default Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+const PopupHandler = ({ geoJsonData }) => {
   const map = useMap();
 
   useEffect(() => {
@@ -65,19 +74,6 @@ const PopupHandlerClient = ({ geoJsonData }) => {
 };
 
 const MapComponent = ({ geoJsonData }) => {
-  // Konfigurasi icon default Leaflet
-  useEffect(() => {
-    delete L.Icon.Default.prototype._getIconUrl;
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-      iconUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-      shadowUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-    });
-  }, []);
-
   return (
     <MapContainer
       center={[-0.7893, 113.9213]}
@@ -93,8 +89,10 @@ const MapComponent = ({ geoJsonData }) => {
       <LeafletControlGeocoder />
       <ScaleControl position="bottomleft" />
       <FeatureGroup>
-        <PopupHandlerClient geoJsonData={geoJsonData} />
-        <DrawComponent onGeoJSONChange={geoJsonData} />
+        <PopupHandler geoJsonData={geoJsonData} />
+        <DrawComponent
+          onGeoJSONChange={geoJsonData}
+        />
       </FeatureGroup>
     </MapContainer>
   );
